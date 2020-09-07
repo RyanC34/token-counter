e450a97183428a25436ce18a1366909555d0b302
RyanC34 <Ryan.chan07@icloud.com>
2020-09-07 08:32:08 -0400

    finished player controls, core functionality complete

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -5,12 +5,16 @@ import '../App.css';
 
 function PlayerCard(props) {
 
+    var {
+        pMoney, betMoney, add100, winMoney
+    } = props;
+
     return (
         <div className='test-class'>
             <Input placeholder='Enter New Name' />
             <Text h2>Player 1</Text>
-            <Text h3>{props.pMoney}</Text>
-            <PlayerControls/>
+            <Text h3>{pMoney}</Text>
+            <PlayerControls betMoney={betMoney} add100={add100} winMoney={winMoney}/>
         </div>
     )
 }
--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { Button } from '@geist-ui/react';
 import '../App.css'
 
-function PlayerControls() {
+
+function PlayerControls(props) {
+    var{
+        betMoney, add100, winMoney
+    } = props;
+
     return (
         <div className='PlayerControlBox'>
             <div className='TopBox'>
-                <Button className="ButtonSpace" auto type='error'>Bet(-100)</Button>
-                <Button className="ButtonSpace" auto type='secondary'>Add(+100)</Button>
+                <Button className="ButtonSpace" auto type='error' onClick={betMoney}>Bet(-100) </Button>
+                <Button className="ButtonSpace" auto type='secondary' onClick={add100}>Add(+100)</Button>
             </div>
             
-            <Button className="ButtonSpace" auto type='success'>Win Pot</Button>
+            <Button className="ButtonSpace" auto type='success' onClick={winMoney}>Win Pot</Button>
         </div>
     )
 }
